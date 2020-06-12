fab674296cb9edac914dae247adad8961c37e4ae
DeJa <barclay.deja@gmail.com>
2020-06-12 00:10:55 -0500

    Updates

---
+++ precache-manifest.71a2bd1229796d39a6819135e6f2d24a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6885f48a6ad2ef456a07f4a246c8131b",
+    "url": "/React-Tip-Calculator/index.html"
+  },
+  {
+    "revision": "67eb46cf6bd2d94948ef",
+    "url": "/React-Tip-Calculator/static/css/main.1970ed22.chunk.css"
+  },
+  {
+    "revision": "8cf8c9c1a74728056e6b",
+    "url": "/React-Tip-Calculator/static/js/2.0f008992.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tip-Calculator/static/js/2.0f008992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67eb46cf6bd2d94948ef",
+    "url": "/React-Tip-Calculator/static/js/main.bd5cc5f3.chunk.js"
+  },
+  {
+    "revision": "f02703de5b7805b4722d",
+    "url": "/React-Tip-Calculator/static/js/runtime-main.6327347e.js"
+  }
+]);
